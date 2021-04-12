58edcab053b072b8c3a01799c9dabee46fa48ec9
Samer Odeh <samerodeh220@gmail.com>
2021-04-12 21:43:33 +0300

    new project for lab 02

---
+++ js/app.js
@@ -0,0 +1,206 @@
+  'use strict' 
+
+  const userName = prompt('please ente your name')
+    
+    function printUseName (name) {
+   
+        console.log('userName var = ', userName);
+    
+    alert ('Hello welcome to my site have a nice day ' + userName );
+} 
+
+  printUseName(userName);
+
+
+      let userInput = prompt('do you  expect i finished my study ?').toLowerCase();
+      
+      
+      
+      function printUserInput (Input) {
+   
+           console.log('userInput var =',userInput); 
+      
+ } 
+
+ /*while (userInput !== 'yes' && userInput !== '') {
+    userInput = prompt('please choose yes or no');
+ }*/
+
+  /*switch(userInput) {
+     case 'yes':
+      alert ('wrong answer'); 
+    break; 
+     case 'no':
+      alert ('right answer');
+    break; 
+     default:
+      alert ('please chose yes or no');
+    break;
+ }*/
+
+
+    
+    
+    let userAnswer = prompt('Do you  expect I am over 25 years old ?').toLowerCase();
+    
+    
+    function printUserAnswer (answer) {
+   
+        console.log('userAnswer var =', userAnswer);
+
+  
+
+ }    
+
+  /*switch(userAnswer) {
+    case 'yes':
+     alert ('wrong answer'); 
+   break; 
+    case 'no':
+     alert ('right answer');
+   break; 
+    default:
+     alert ('please chose yes or no');
+   break;
+ }*/
+    
+    
+   let userOpinion = prompt('Do you think I am studying programming ?').toLowerCase();
+      
+    function printUserOpinion (opinion) {
+   
+         console.log('userOpinion var =', userOpinion);
+
+    
+    
+ } 
+
+
+  /*switch(userOpinion) {
+    case 'yes':
+     alert ('right answer'); 
+   break; 
+    case 'no':
+     alert ('wrong answer');
+   break; 
+    default:
+     alert ('please chose yes or no');
+   break;
+  } */
+
+    
+   let userThink = prompt('Do you think I studied a field other than programming ?').toLowerCase();
+      
+    function printUserThink (think) {
+   
+         console.log('userThink var =', userThink);
+
+    
+    } 
+
+    
+  /*switch(userThink) {
+    case 'yes':
+     alert ('right answer'); 
+   break; 
+    case 'no':
+     alert ('wrong answer');
+   break; 
+    default:
+     alert ('please chose yes or no');
+   break;
+  }*/
+
+   
+   let userExpect = prompt('Do you think I studied programming in English ?').toLowerCase();
+      
+    function printUserExpect (expect) {
+   
+         console.log('userExpect var=', userExpect);
+
+    
+    }
+
+    
+  /*switch(userExpect) {
+    case 'yes':
+     alert ('right answer'); 
+   break; 
+    case 'no':
+     alert ('wrong answer');
+   break; 
+    default:
+     alert ('please chose yes or no');
+   break;
+  }*/
+    
+    
+
+
+  switch(userInput) {
+   case 'yes':
+    alert ('wrong answer'); 
+  break; 
+   case 'no':
+    alert ('right answer');
+  break; 
+   default:
+    alert ('please choose yes or no');
+  break;
+}
+
+    
+
+switch(userAnswer) {
+    case 'yes':
+     alert ('wrong answer'); 
+   break; 
+    case 'no':
+     alert ('right answer');
+   break; 
+    default:
+     alert ('please choose yes or no');
+   break;
+ }
+
+ switch(userOpinion) {
+    case 'yes':
+     alert ('right answer'); 
+   break; 
+    case 'no':
+     alert ('wrong answer');
+   break; 
+    default:
+     alert ('please choose yes or no');
+   break;
+  }
+ 
+  switch(userThink) {
+    case 'yes':
+     alert ('right answer'); 
+   break; 
+    case 'no':
+     alert ('wrong answer');
+   break; 
+    default:
+     alert ('please choose yes or no');
+   break;
+  } 
+
+  switch(userExpect) {
+    case 'yes':
+     alert ('right answer'); 
+   break; 
+    case 'no':
+     alert ('wrong answer');
+   break; 
+    default:
+     alert ('please choose yes or no');
+   break;
+  }
+
+
+
+printUseName(userName);
+console.log('don’t delete');
+alert ('don’t delete');
